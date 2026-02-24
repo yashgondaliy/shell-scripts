c7c3af1920a863517134fff7d67773eaf94535a6
Ubuntu <ubuntu@ip-172-31-6-51.eu-west-1.compute.internal>
2026-02-24 10:19:12 +0000

    added 3 idiots scripts

---
+++ 3_idiots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#user defined variable
+
+# ./3idiots.sh rancho raju farhan
+
+hero="$1"
+villain="$2"
+
+echo "3 idiots hero is $hero"
+
+echo "3 idiots villain is $villain"
+
+# pre defined variable
+
+echo "current loged user $USER"
+
+echo "total number of variable $#"
