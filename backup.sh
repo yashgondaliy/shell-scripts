6ed6fe8ffe77aad3c238d17c7d19ad07d02d51ac
yashgondaliy <yashgondaliya0@gmail.com>
2026-02-24 10:24:19 +0000

    added backup scripts

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+<<info
+this file is used to backup 
+info
+
+src=$1
+dest=$2
+timestamp=$(date '+%y-%m-%d-%H:%M:%S')
+
+zip -r "$dest/backup-$timestamp.zip" $src
+
+aws s3 sync $dest s3://aws-backups-y
+
+echo "successfully backup completed & Stored in s3"
+
+
